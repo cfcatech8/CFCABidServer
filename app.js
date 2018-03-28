4aec820b5125072d5dd0181eac59ffa187f03158
cgao6 <gps949@outlook.com>
2018-03-28 10:53:32 +0800

    Imported the RAE template

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var login = require('./routes/login');
+var index = require('./routes/index');
+var logout = require('./routes/logout');
+var certApply = require('./routes/certApply');
+var certDownload = require('./routes/certDownload');
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -13,18 +18,28 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+//app.all('*', login.Auth);
+app.use('/', index);
+//app.use('/users', users);
+app.use('/login', login);
+app.use('/logout', logout);
+app.use('/certApply',certApply);
+app.use('/certDownload',certDownload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
